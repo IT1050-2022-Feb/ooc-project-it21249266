f83bbe4600ccb454449f634e51a5dfd93fa61a43
IT21562488 <99161794+IT21562488@users.noreply.github.com>
2022-05-17 00:14:43 +0530

    Add Reservation.cpp

---
+++ Reservation.cpp
@@ -0,0 +1,57 @@
+//created by IT21562488 - A.H.S ANJANA
+
+#include "Reservation.h"
+#include<iostream>
+#include <string>
+using namespace std;
+
+
+Reservation::Reservation()
+{
+	reservationNo = 00;
+	reservationDate = "DD/MM/YYYY";
+	mealType = " ";
+	seatType = " ";
+	
+}
+
+Reservation::Reservation(int rNo,string rDate,string mType,string sType)
+{
+
+	reservationNo = rNo;
+	reservationDate = rDate;
+	mealType = mType;
+	seatType = sType;
+	
+	
+}
+void Reservation::addTicket(Ticket *t)
+{
+	
+}
+void Reservation::addPassenger(Passenger *p)
+{
+	
+}
+void Reservation::calcelReservation ()
+{
+	
+}
+void Reservation::validateReservation ()
+{
+	
+}
+void Reservation::displayReservationDetails ()
+{
+	
+}
+void Reservation::displayTickect ()
+{
+	
+	tic -> displayTicketDetails();
+}
+Reservation::~Reservation ()
+{
+	cout<<"Reservation shutting down"<<endl;
+	delete tic;
+}
