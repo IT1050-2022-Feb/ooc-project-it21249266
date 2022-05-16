e0bbceeb60dfc2a5270d2262f4a5d5150d2f87db
Sayuru Bopitiya <42121050+SayuruRehan@users.noreply.github.com>
2022-05-16 15:40:43 +0000

    Added CardPayment and Payment-IT21470004

---
+++ Payment.cpp
@@ -0,0 +1,48 @@
+//Created by IT21470004 - S. R. Bopitiya
+
+#include <iostream>
+#include "Payment.h"
+using namespace std;
+
+Payment::Payment()
+{
+  payID = "";
+  payType = "";
+  totalPayment = 0;
+}
+
+Payment::Payment(int p_payID, int p_PID, string pay_pType, string pay_pDate, float pay_totPay)
+{
+  payID = p_payID;
+  passengerID = p_PID;
+  payType = pay_pType;
+  payDate = pay_pDate;
+  totalPayment = pay_totPay;
+}
+
+void Payment::setPaymentDetails(int payID, string payType, float totalPayment)
+{
+  
+}
+
+void Payment::getPaymentDetails()
+{
+  cout << "Payment ID: " << payID << endl;
+  cout << "Payment Type: " << payType << endl;
+  cout << "Payment Amount: " << TotalPayment << endl;
+}
+
+void Payment::confirmPaymentDetails()
+{
+  
+}
+
+void Payment::validatePaymentDetails()
+{
+  
+}
+
+Payment::~Payment()
+{
+  cout << "Deleting Payments" << endl;
+}
---
+++ Payment.h
@@ -0,0 +1,26 @@
+//Created by IT21470004 - S. R. Bopitiya
+
+#pragma once
+#include "Passenger.h"
+#include "Staff.h"
+#include "Report.h"
+#include <string>
+using namespace std;
+
+class Payment
+{
+    protected:
+      int payID;
+      string payType;
+      float totalPayment;
+      Passenger* p;    //passenger dynamic object
+
+    public:
+      Payment();    //constructor
+      Payment(int p_payID; int p_PID, string pay_pType, string pay_pDate, float pay_totPay);
+      void setPaymentDetails(int payID, string payType, float totalPayment);
+      float getPaymentDetails();
+      void confirmPaymentDetails();
+      void validatePaymentDetails();
+      ~Payment();    //destructor
+}
